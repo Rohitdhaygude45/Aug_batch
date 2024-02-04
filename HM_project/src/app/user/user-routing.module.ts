f8e6af85ab606ed1a9b5f07cc26b221bec06f7af
Rohitdhaygude45 <rohitdhaygude45@gmail.com>
2024-02-04 19:38:23 +0530

    user journey

--- HM_project/src/app/user/user-routing.module.ts
+++ HM_project/src/app/user/user-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookNowComponent } from './book-now/book-now.component';
 import { UserSuccessComponent } from './user-success/user-success.component';
 import { UserlandingComponent } from './userlanding/userlanding.component';
 
 const routes: Routes = [ 
   {path:'userLanding', component:UserlandingComponent},
-  { path:'userSuccess',component:UserSuccessComponent}
+  { path:'userSuccess',component:UserSuccessComponent},
+  { path:'bookNow',component:BookNowComponent}
 ];
 
 @NgModule({
@@ -13,3 +15,7 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class UserRoutingModule { }
+
+
+
+
--- HM_project/src/app/user/user-success/user-success.component.ts
+++ HM_project/src/app/user/user-success/user-success.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApicallService } from 'src/app/shared/apicall.service';
 
 @Component({
   selector: 'app-user-success',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-success.component.css']
 })
 export class UserSuccessComponent {
+
+  hotelData: any;
+
+
+  showData:Boolean =false;
+    headers =['ownerName','mob','hotelName','hotelMenu','hotelAddress','hotelContact','Book Now']
+  
+  
+  constructor( private apicallService:ApicallService){}
+  
+    
+  ngOnInit(){
+    
+    this.getHotelDetails();
+  }
+  
+  
+  async getHotelDetails(){
+  
+    this.hotelData = await this.apicallService.getApiCall('hotelDetails').toPromise()
+     
+      console.log( "hotelData" ,this.hotelData);
+      
+    }
+    viewHotels(){
+      this.showData = true;
+    }
+  
+
+
+
 
 }
